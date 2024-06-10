4ea703167577467636624276d74414340d4b3c25
HappyHarry1130 <andril.com@outlook.com>
2024-06-09 19:16:05 -0700

    update_date

--- runMajesticDownload.sh
+++ runMajesticDownload.sh
@@ -5,7 +5,7 @@ DL_DIR_NAME=majestic_downloader
 DL_BQ_DATASET_NAME=Majestic
 DL_BQ_TABLE_NAME=Majestic
 DL_CSV_DIR_NAME=output
-DL_GS_BUCKET_NAME=gs://statbid1/statbid-argos-datacollection-prod/knifecenter/majestic
+DL_GS_BUCKET_NAME=gs://statbid-argos-datacollection-prod1/knifecenter/majestic
 
 gcloud auth activate-service-account --key-file=../feed-storage-94d94e70e271.json
 
